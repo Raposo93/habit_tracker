20e09b55e65d1588386cfa3ecf1a030f5f440614
Gonzalo <gonzalorave7@gmail.com>
2026-05-27 22:03:30 +0200

    Initialize Java Maven project

---
+++ src/test/java/com/raposo/habittracker/SanityTest.java
@@ -0,0 +1,13 @@
+package com.raposo.habittracker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SanityTest {
+
+    @Test
+    void projectRunsTests() {
+        assertTrue(true);
+    }
+}
