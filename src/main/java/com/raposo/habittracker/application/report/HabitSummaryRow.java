1cae83b1af839644949f12ccfdfc7de717c42dd5
Gonzalo <gonzalorave7@gmail.com>
2026-06-03 21:47:55 +0200

    Feat: Add basic habit summary row

--- src/main/java/com/raposo/habittracker/application/report/HabitSummaryRow.java
+++ src/main/java/com/raposo/habittracker/application/report/HabitSummaryRow.java
@@ -2,9 +2,8 @@ package com.raposo.habittracker.application.report;
 
 public record HabitSummaryRow(
         String habit,
-        double previousAvg,
-        double currentAvg,
-        double delta,
-        Trend trend
+        double averageScore,
+        int recordedDays,
+        int missingDays
 ) {
 }
